3afabfcc40498b2e152c26cd8ff2fb50c25cf312
alakambhavya <alakambhavya@gmail.com>
2026-05-22 18:25:57 +0530

    explining about tasks

---
+++ operator.py
@@ -0,0 +1,55 @@
+# task3:Write a program to perform all arithmetic operations dynamically.  
+num_1=20
+num_2=30
+sum=num_1+num_2
+print(sum)
+sub=num_1-num_2
+print(sub)
+mul=num_1*num_2
+print(mul)
+div=num_1/num_2
+print(div)
+mod=num_1%num_2
+print(mod)
+floor_division=num_1//num_2
+print(floor_division)
+exp=num_1**num_2
+print(exp)
+
+print("...................")
+
+#task4: Build a Boolean condition checker using logical operators.  
+print(49.9>34 and 80<100) 
+print(50>30 or 89.6<20) 
+print( not 50>=20) 
+
+print(".......................")
+
+#task5:Create a binary calculator using bitwise operations.  
+num_3=0
+num_4=1
+print(num_3&num_4)
+print(num_3|num_4)
+print(num_3^num_4)
+print(~num_4)
+print(num_3<<num_4)
+print(num_3>>num_4)
+
+print(".....................")
+
+#task2:Create a program to validate email usernames using identifiers.  
+user1="alakambhavya@gmail.com"
+user2=user1
+print(user1 is user2)
+print(user1 is not user2)  
+
+
+
+
+
+
+
+ 
+
+
+
