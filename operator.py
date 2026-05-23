3b381b18b7719a8d98f41ecd7c95a437a1b4a1b7
alakambhavya <alakambhavya@gmail.com>
2026-05-23 10:31:54 +0530

    operators

--- operator.py
+++ operator.py
@@ -1,3 +1,21 @@
+#task1:Build a mini search engine using membership operators.
+names="bhavya","suchi","varnika","nandu"
+search=input("enter data")
+search="nandu"
+print(search in names)
+print(search not in names)
+
+print("____________")
+
+#task2:Create a program to validate email usernames using identifiers.  
+user1="alakambhavya@gmail.com"
+user2=user1
+user_input=input("enter mail")
+print(user1 is user2)
+print(user1 is not user2)
+
+print("___________") 
+
 # task3:Write a program to perform all arithmetic operations dynamically.  
 num_1=20
 num_2=30
@@ -16,14 +34,14 @@ print(floor_division)
 exp=num_1**num_2
 print(exp)
 
-print("...................")
+print("_____________")
 
 #task4: Build a Boolean condition checker using logical operators.  
 print(49.9>34 and 80<100) 
 print(50>30 or 89.6<20) 
 print( not 50>=20) 
 
-print(".......................")
+print("_______________")
 
 #task5:Create a binary calculator using bitwise operations.  
 print("binary calculator")
@@ -36,23 +54,6 @@ print(~num_4)
 print(num_3<<num_4)
 print(num_3>>num_4)
 
-print(".....................")
-
-#task2:Create a program to validate email usernames using identifiers.  
-user1="alakambhavya@gmail.com"
-user2=user1
-user_input=input("enter mail")
-print(user1 is user2)
-print(user1 is not user2)  
-
-print(".............")
-
-#task1:Build a mini search engine using membership operators.
-names="bhavya","suchi","varnika","nandu"
-search=input("enter data")
-search="nandu"
-print(search in names)
-print(search not in names)
  
 
 
