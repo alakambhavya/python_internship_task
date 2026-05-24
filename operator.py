a2fa844c380059f9436d69f442ad7db7e901339f
alakambhavya <alakambhavya@gmail.com>
2026-05-24 20:02:52 +0530

    operators task

--- operator.py
+++ operator.py
@@ -1,6 +1,5 @@
 #task1:Build a mini search engine using membership operators.
 names="bhavya","suchi","varnika","nandu"
-search=input("enter names")
 search="nandu"
 print(search in names)
 print(search not in names)
